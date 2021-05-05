a5e4d0fbe4e2973910a669d57ee416bb61329c76
Beknaz Baktygulov <enelbbs9@gmail.com>
2021-05-05 12:01:58 -0700

    added key property since rendering a list, smoother transition when renders

--- src/Results.js
+++ src/Results.js
@@ -14,12 +14,12 @@ function Results({ selectedOption }) {
             return request;
         }
         fetchData();
-    }, [])
+    }, [selectedOption])
 
     return (
         <div className="results">
             {movies.map((movie) => (
-                <VideoCard movie={movie}/>
+                <VideoCard key={movie.id} movie={movie}/>
             ))}
         </div>
     );
