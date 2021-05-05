11fc6f8b8f788d3d40a3c06c30dc5d02b18e6a38
Beknaz Baktygulov <enelbbs9@gmail.com>
2021-05-05 11:45:53 -0700

    Added likes functionality and hover effects to videocard

--- src/VideoCard.js
+++ src/VideoCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './VideoCard.css';
 import TextTruncate from "react-text-truncate";
+import { ThumbUpSharp } from "@material-ui/icons";
 
 const base_url = "https://image.tmbd.org/t/p/original/";
 
@@ -9,9 +10,9 @@ function VideoCard({ movie }) {
         <div className="videoCard">
             <img src={`${base_url}${movie.backdrop_path || movie.poster_path}`} alt="movie poster"/>
             <TextTruncate line={1} element="p" truncateText="..." text={movie.overview} />
-            <p>{movie.release_date || movie.first_air_date}</p>
             <h2>{movie.title || movie.original_name}</h2>
-            <p>Number of likes...</p>
+            <p className="videoCard__stats">{movie.media_type && `${movie.media_type}`}{movie.release_date || movie.first_air_date} <ThumbUpSharp />{movie.vote_count}</p>
+            
         </div>
     )
 }
